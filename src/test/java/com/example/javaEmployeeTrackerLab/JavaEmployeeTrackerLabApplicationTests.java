34dfb928d0c77ceabf3c7b2da6cd22c60d0cfc08
John <ashton.basile@gmail.com>
2023-09-28 12:28:43 +0100

    The setup is complete

--- src/test/java/com/example/javaEmployeeTrackerLab/JavaEmployeeTrackerLabApplicationTests.java
+++ src/test/java/com/example/javaEmployeeTrackerLab/JavaEmployeeTrackerLabApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.javaEmployeeTrackerLab;
 
+import com.example.javaEmployeeTrackerLab.models.Employee;
+import com.example.javaEmployeeTrackerLab.repositories.EmployeeRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JavaEmployeeTrackerLabApplicationTests {
 
+	@Autowired
+	EmployeeRepository employeeRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void createEmployee() {
+		Employee john = new Employee("John", 31, 47, "john@yahoo.com");
+		employeeRepository.save(john);
 	}
 
 }
