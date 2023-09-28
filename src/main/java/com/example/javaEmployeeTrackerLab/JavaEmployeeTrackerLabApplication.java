b2c0f5b1b0b6e4eaa37ba1dfc0b6f37e64294859
John <ashton.basile@gmail.com>
2023-09-28 11:40:49 +0100

    first commit

---
+++ src/main/java/com/example/javaEmployeeTrackerLab/JavaEmployeeTrackerLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaEmployeeTrackerLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEmployeeTrackerLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEmployeeTrackerLabApplication.class, args);
+	}
+
+}
