c9b1d7e8a8718ccceb63bb025e3a8b2eb17dc0be
John <ashton.basile@gmail.com>
2023-10-02 14:59:50 +0100

    added a test of the project

--- src/test/java/com/example/javaEmployeeTrackerLab/JavaEmployeeTrackerLabApplicationTests.java
+++ src/test/java/com/example/javaEmployeeTrackerLab/JavaEmployeeTrackerLabApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.javaEmployeeTrackerLab;
 
 import com.example.javaEmployeeTrackerLab.models.Department;
 import com.example.javaEmployeeTrackerLab.models.Employee;
+import com.example.javaEmployeeTrackerLab.models.Project;
 import com.example.javaEmployeeTrackerLab.repositories.DepartmentRepository;
 import com.example.javaEmployeeTrackerLab.repositories.EmployeeRepository;
+import com.example.javaEmployeeTrackerLab.repositories.ProjectRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +20,9 @@ class JavaEmployeeTrackerLabApplicationTests {
 
 	@Autowired
 	DepartmentRepository departmentRepository;
+
+	@Autowired
+	ProjectRepository projectRepository;
 
 	@Test
 	void contextLoads() {
@@ -36,6 +41,22 @@ class JavaEmployeeTrackerLabApplicationTests {
 		department.setEmployees(listOfEmployees);
 
 		System.out.println(department.getEmployees());
+	}
+
+	@Test
+	public void addEmployeesAndProjects(){
+		Department department = new Department("H.R.");
+		departmentRepository.save(department);
+
+		Employee employee = new Employee("Jack", 31, 3298, "email@email.com", department);
+		employeeRepository.save(employee);
+
+		Project project = new Project("Impossible Mission", 150);
+		projectRepository.save(project);
+
+		project.addEmployee(employee);
+		projectRepository.save(project);
+
 	}
 
 }
