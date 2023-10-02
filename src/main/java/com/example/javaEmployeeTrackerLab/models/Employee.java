d9995775e4750aa237f5e3c2ae17a0a87182229e
John <ashton.basile@gmail.com>
2023-10-02 14:38:49 +0100

    added getters and setters for department

--- src/main/java/com/example/javaEmployeeTrackerLab/models/Employee.java
+++ src/main/java/com/example/javaEmployeeTrackerLab/models/Employee.java
@@ -122,4 +122,12 @@ public class Employee {
     public void setProjects(List<Project> projects) {
         this.projects = projects;
     }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
 }
